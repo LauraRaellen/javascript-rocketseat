c8e3e6b4da8dcfb7b62ff1702ca48ec8ef935fda
Laura Raellen <lauraraellen0@gmail.com>
2022-07-08 23:34:11 -0300

    Mais sobre function

---
+++ lembrando.js
@@ -0,0 +1,54 @@
+// let age = 18;
+// let user = "Laura";
+
+// console.log(`o ${user} tem ${age} anos`);
+
+// //================================================
+
+// const animals = [
+//     {
+//         name: "Lion",
+//         age: 3
+//     },
+
+//     {
+//         name: "Cat",
+//         age: 2
+//     }
+//  ]
+
+//==================================================
+/* Pratica */
+
+//1. Declare uma variável de nome weight
+// let = weight
+
+//2. Que tipo de dado é a variável acima? É um undefined, pela causa de não ter colocado um valor nela.
+
+// 3. Declare uma variável e atribua valores para cada um dos dados:
+
+let name = "Laura";
+let age = 13;
+let stars = 4.3;
+let isSubscribed = true;
+
+/*
+    4. A variável student abaixo é de que tipo de dados? Um object
+
+    4.1 Atribua a ela as mesmas propriedades e valores do exercícios 3.
+
+    4.2 Mostre no console a seguinte mensagem:
+
+    <name> tem <age> anos e pesa <weight>kg.
+*/
+
+let student = {
+
+    name: "Laura",
+    age: 13,
+    stars: 4.3,
+    isSubscribed: true, 
+    weight: 4.8
+}
+
+console.log(`${student.name} tem ${student.age} anos e pesa ${student.weight}kg.`);
