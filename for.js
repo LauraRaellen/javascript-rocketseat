7cc2fa3b9bd6bccbcb2401c5c1072d2e121b154e
Laura Raellen <lauraraellen0@gmail.com>
2022-07-15 11:56:39 -0300

    Aula de Estrutura de repetição for

---
+++ for.js
@@ -0,0 +1,12 @@
+//Estrutura de repetição
+//for 
+//break - para a execução do loop
+//continue - pula a excuçãodo momento
+
+for(let i = 10; i >= 0; i--) {
+    if(i === 5) {
+        continue;
+    }
+
+    console.log(i);
+}
